5d074a00991d676b859b89f763ed5f17d6fbc44d
amemariee <awall@csumb.edu>
2022-09-28 16:04:12 -0700

    Added button for random student

--- WaSP/WaSP/ContentView.swift
+++ WaSP/WaSP/ContentView.swift
@@ -10,10 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Class One")
+            Button("Random Student") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            //Image(systemName: "globe")
+               // .imageScale(.large)
+                //.foregroundColor(.accentColor)
+            
         }
         .padding()
     }
