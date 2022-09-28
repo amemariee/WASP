f06b303ac88a3e49f6b534812443266ee8d782ad
amemariee <awall@csumb.edu>
2022-09-28 15:38:45 -0700

    hello world app

---
+++ WaSP/WaSP/WaSPApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaSPApp.swift
+//  WaSP
+//
+//  Created by user228393 on 9/28/22.
+//
+
+import SwiftUI
+
+@main
+struct WaSPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
