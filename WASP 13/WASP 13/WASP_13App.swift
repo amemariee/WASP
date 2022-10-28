01daf34c77cb6e83998e47ff6348ba3878bbfd14
RajveerSaini02 <79181225+RajveerSaini02@users.noreply.github.com>
2022-10-28 10:42:04 -0700

    Add files via upload

---
+++ WASP 13/WASP 13/WASP_13App.swift
@@ -0,0 +1,17 @@
+//
+//  WASP_13App.swift
+//  WASP 13
+//
+//  Created by Rajveer Saini on 10/28/22.
+//
+
+import SwiftUI
+
+@main
+struct WASP_13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
